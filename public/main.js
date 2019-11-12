91e94a139794ca14eb69a838c6b835d7ce21670a
Reid Hanson <reidhans@buffalo.edu>
2019-11-12 16:19:17 -0500

    first draft

--- public/main.js
+++ public/main.js
@@ -1,7 +1,29 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const getWeather = async () => {
+  const city = document.querySelector('.zip').value
+  const response = await fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      city +
+      '&appid=658ce6b7e5a6aba1b49ed129b567beba&units=imperial'
+  )
+  const weatherData = await response.json()
+  displayData(
+    weatherData.main.temp +
+      ' Degrees and ' +
+      weatherData.weather[0].main +
+      ' with a high of ' +
+      weatherData.main.temp_max +
+      ' and a low of ' +
+      weatherData.main.temp_min
+  )
+  if (weatherData.main.temp > 80) {
+    document.querySelector('.description').textContent = 'WOW HOT!'
+  } else if (weatherData.main.temp < 80) {
+    document.querySelector('.description').textContent = 'Not so bad'
   }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const displayData = weatherData => {
+  document.querySelector('.info').textContent = weatherData
+}
+
+document.querySelector('.search').addEventListener('click', getWeather)
